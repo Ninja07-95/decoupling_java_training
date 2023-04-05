0e338969890f634b8d065c7a4dfdaa3f10d2bd9e
Anass Barriz <anass1.barriz@epita.fr>
2023-04-05 15:53:15 +0200

    Setup Part7

--- src/main/java/fr/lernejo/guessgame/HumanPlayer.java
+++ src/main/java/fr/lernejo/guessgame/HumanPlayer.java
@@ -2,15 +2,16 @@ package fr.lernejo.guessgame;
 
 import fr.lernejo.logger.Logger;
 import fr.lernejo.logger.LoggerFactory;
+
 import java.util.Scanner;
 
 public class HumanPlayer implements Player {
     private final Logger logger = LoggerFactory.getLogger("player");
+
     @Override
     public long askNextGuess() {
         Scanner scanner = new Scanner(System.in);
-        while (true)
-        {
+        while (true) {
             logger.log("Enter a guess:");
             if (!scanner.hasNextLong()) {
                 logger.log("Input must be a long number");
@@ -21,14 +22,15 @@ public class HumanPlayer implements Player {
             scanner.nextLine();
             return num;
         }
-
     }
 
     @Override
     public void respond(boolean lowerOrGreater) {
-        if (lowerOrGreater)
-            logger.log("The value to find is higher" );
-        else
+        if (lowerOrGreater) {
+            logger.log("The value to find is higher");
+        } else {
             logger.log("The value to find is lower");
+        }
     }
 }
+
