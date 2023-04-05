8eb49eb77c57b968836924f2d7bda07dea16fcf0
Anass Barriz <anass1.barriz@epita.fr>
2023-04-05 14:49:05 +0200

    Setup Part4

--- src/main/java/fr/lernejo/logger/LoggerFactory.java
+++ src/main/java/fr/lernejo/logger/LoggerFactory.java
@@ -1,9 +1,14 @@
 package fr.lernejo.logger;
 
+import java.util.function.Predicate;
+
 public class LoggerFactory {
 
-    public static Logger getLogger(Class<?> clazz) {
-        return new ConsoleLogger(clazz);
+    public static Logger getLogger(String name){
+        Predicate<String> condition = message->message.contains("simulation");
+        return new ContextualLogger(name,
+            new CompositeLogger(new ConsoleLogger(),
+                                new FilteredLogger(new FileLogger("logs"), condition)));
     }
 }
 
--- src/main/java/fr/lernejo/logger/logger.java
+++
@@ -1,6 +0,0 @@
-package fr.lernejo.logger;
-
-public interface Logger {
-
-    void log(LogLevel level, String message);
-}
